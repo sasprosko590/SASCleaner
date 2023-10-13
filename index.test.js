e154c57cd1028c0a76206b9e596d667805208f4f
Umut <77373632+sasprosko590@users.noreply.github.com>
2023-10-13 16:22:27 +0300

    index.test.js

--- index.test.js
+++ index.test.js
@@ -1,12 +1,23 @@
-const cleaner = require("./index.js");
+const cleaner = require("./index");
 
-try {
-  cleaner.clear({
-    openDiskCleaner: true,
-    openMRT: false,
-    openSFC: false,
-    openDism: false,
-  });
-} catch (error) {
-  console.error("An error occurred:", error.message);
+async function runExample() {
+  try {
+    console.log("Running system cleanup and optimization script...");
+
+    const options = {
+      openDiskCleaner: true,
+      openMRT: true,
+      openSFC: true,
+      openDismRepair: true,
+      openDismGetPackages: true,
+      openDismAddPackages: true,
+    };
+    await cleaner.clear(options);
+
+    console.log("System cleanup and optimization completed successfully!");
+  } catch (error) {
+    console.error("Error during system cleanup and optimization:", error.message);
+  }
 }
+
+runExample();
