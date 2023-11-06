da14304b554c281a3717acdfe512e1d208226820
Umut <77373632+sasprosko590@users.noreply.github.com>
2023-11-06 18:24:26 +0300

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,31 +1,29 @@
 const cleaner = require("../src/index.js");
-const error = require("../errors/error.js").logError;
+const logError = require("../errors/error.js").logError;
 const fs = require("fs");
 async function runExample() {
   try {
-    console.log("Running SASPClean script...");
-
     const options = {
-      clearSpotifyData: true,
-      clearWindows10Upgrade: true,
-      clearWindowsOld: true,
-      clearWindowsUpdate: true,
-      openCDF: true,
-      openCDR: true,
-      openCDX: true,
-      openDiskCleaner: true,
-      openDiskCleanerSageRun: true,
-      openDismAddPackages: true,
-      openDismCheckHealth: true,
-      openDismGetPackages: true,
-      openDismRepair: true,
-      openDismRestoreHealth: true,
-      openMDT: true,
-      openMRT: true,
-      openSFC: true,
-      openWF: true,
-      openWingetUpgrade: true,
-      updateCheckWindowsUpdate: true,
+      clearSpotifyData: false,
+      clearWindows10Upgrade: false,
+      clearWindowsOld: false,
+      clearWindowsUpdate: false,
+      openCDF: false,
+      openCDR: false,
+      openCDX: false,
+      openDiskCleaner: false,
+      openDiskCleanerSageRun: false,
+      openDismAddPackages: false,
+      openDismCheckHealth: false,
+      openDismGetPackages: false,
+      openDismRepair: false,
+      openDismRestoreHealth: false,
+      openMDT: false,
+      openMRT: false,
+      openSFC: false,
+      openWF: false,
+      openWingetUpgrade: false,
+      updateCheckWindowsUpdate: false,
     };
 
     cleaner.clear(options);
