0a1ec890dc6b1dd3e04b0a1ac9377e703a9c81a3
Umut <77373632+sasprosko590@users.noreply.github.com>
2023-11-28 12:29:29 +0300

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-set "scriptName=SASPClean.ps1"
-set "currentDir=%~dp0"
-set "scriptPath=!currentDir!!scriptName!"
-powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "!scriptPath!"
-endlocal
